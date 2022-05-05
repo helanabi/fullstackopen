8d478f78198ef81d3efb838e631296b4d2b9a873
Hassan El Anabi <helanabi@gmail.com>
2022-05-05 19:11:25 +0000

    finish exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
 
+const Statistics = ({ good, neutral, bad }) => {
+    const total = good + neutral + bad;
+    
+    return (
+        <div>
+            <p>Good { good }</p>
+            <p>Neutral { neutral }</p>
+            <p>Bad { bad }</p>
+            <p>All { total }</p>
+            <p>Average { (good - bad) / total }</p>
+            <p>Positive { good*100 / total }%</p>
+        </div>
+    );
+};
+
 const App = () => {
 
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-
-    const total = good + neutral + bad;
 
     return (
         <div>
@@ -17,13 +30,7 @@ const App = () => {
             <button onClick={ () => setBad(bad+1) }>Bad</button>
 
             <h2>Statistics</h2>
-
-            <p>Good { good }</p>
-            <p>Neutral { neutral }</p>
-            <p>Bad { bad }</p>
-            <p>All { total }</p>
-            <p>Average { (good - bad) / total }</p>
-            <p>Positive { good*100 / total }%</p>
+            <Statistics {...{ good, neutral, bad }} />
         </div>
     );
 };
