f5e4c675a96c30eddb47ec143c201c073be114f1
Hassan El Anabi <helanabi@gmail.com>
2022-05-06 17:49:18 +0000

    finish exercise 2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,39 +1,4 @@
-function Header(props) {
-    return (
-        <h1>{props.course}</h1>
-    );     
-}
-
-function Part(props) {
-    return (
-        <p>
-            {props.name} {props.exercises}
-        </p>
-    );
-}
-
-function Content({parts}) {
-    return (
-        <div>
-            {parts.map(part => <Part key={part.id} {...part} />)}
-        </div>
-    );        
-}
-
-function Course({ course }) {
-    const total = course
-          .parts
-          .map(part => part.exercises)
-          .reduce((x, y) => x+y);
-    
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <p><b>Total of {total} exercises</b></p>
-        </div>
-    );  
-}
+import Course from "./components/course";
 
 function App() {
     const courses = [
