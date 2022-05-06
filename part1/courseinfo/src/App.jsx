dd0f2e57d7fbe4d1dd5b22f486ec529e6f942521
Hassan El Anabi <helanabi@gmail.com>
2022-05-06 17:32:10 +0000

    finish exercise 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -12,52 +12,47 @@ function Part(props) {
     );
 }
 
-function Content(props) {
+function Content({parts}) {
     return (
         <div>
-            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+            {parts.map(part => <Part key={part.id} {...part} />)}
         </div>
     );        
 }
 
-function Total(props) {
+function Course({ course }) {
     return (
-        <p>
-            Number of exercises { props.parts[0].exercises +
-                                  props.parts[1].exercises +
-                                  props.parts[2].exercises }
-        </p>
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+        </div>
     );  
 }
 
 function App() {
     const course = {
+        id: 1,
         name: "Half Stack application development",
         parts: [
             {
                 name: "Fundamentals of React",
-                exercises: 10
+                exercises: 10,
+                id: 1
             },
             {
                 name: "Using props to pass data",
-                exercises: 7
+                exercises: 7,
+                id: 2
             },
             {
                 name: "State of a component",
-                exercises: 14
+                exercises: 14,
+                id: 3
             }
         ]
     };
 
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    );
+    return <Course course={course} />;
 }
 
 export default App;
