c9f8fc793d2419d028a97070d61ce35aa8eb5f7f
Hassan El Anabi <helanabi@gmail.com>
2022-05-10 08:48:34 +0100

    finish exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+function Weather({ data }) {
+  if (data) {
+    return (
+      <div>
+        <p>Temperature: {data.temp} Celccius</p>
+        <img
+          src={`http://openweathermap.org/img/wn/${data.icon}@2x.png`}
+          alt={data.description}
+        />
+        <p>Wind: {data.wind} m/s</p>
+      </div>
+    );
+  } else {
+    return <p>Loading data...</p>;
+  }
+}
+
 function Country({ country }) {
+  const capital = country.capital[0];
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather" +
+          `?q=${capital}` +
+          `&appid=${apiKey}` +
+          `&units=metric`
+      )
+      .then(({ data }) =>
+        setWeather({
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          descriptionn: data.weather[0].description,
+          wind: data.wind.speed,
+        })
+      );
+  }, [capital]);
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -18,6 +57,10 @@ function Country({ country }) {
         ))}
       </ul>
       <p style={{ fontSize: "10rem", margin: "1rem 0" }}>{country.flag}</p>
+      <div>
+        <h2>Weather in {capital}</h2>
+        <Weather data={weather} />
+      </div>
     </div>
   );
 }
