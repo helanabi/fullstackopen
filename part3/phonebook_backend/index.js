54cf50751cc3969e7080ea905c80ef2d09a7d776
Hassan El Anabi <helanabi@gmail.com>
2022-05-15 15:41:20 +0100

    finish exercise 3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,7 +5,20 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan((tokens, req, res) =>
+    [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      req.method === "POST" ? JSON.stringify(req.body) : "",
+    ].join(" ")
+  )
+);
 
 const initialPersons = [
   {
