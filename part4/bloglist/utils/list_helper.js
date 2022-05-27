1d521af1a736d011aa1de5ff8091fa424c9e5891
Hassan El Anabi <helanabi@gmail.com>
2022-05-27 18:55:25 +0100

    finish exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,4 +3,8 @@ const dummy = (blogs) => 1;
 const totalLikes = (blogs) =>
   blogs.map((blog) => blog.likes).reduce((s, i) => i + s, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) =>
+  blogs.find((blog) => blog.likes === Math.max(...blogs.map((b) => b.likes))) ||
+  null;
+
+module.exports = { dummy, totalLikes, favoriteBlog };
