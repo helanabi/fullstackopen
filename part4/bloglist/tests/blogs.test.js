9fe7aa2bd343f52d3797b9bb2cd569fab6de64cf
Hassan El Anabi <helanabi@gmail.com>
2022-05-30 07:09:13 +0100

    finish exercise 4.10

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -28,3 +28,22 @@ test("Blog posts have an 'id' property", async () => {
   const blogs = await api.get("/api/blogs").expect(200);
   expect(blogs.body[0].id).toBeDefined();
 });
+
+test("A new blog post can be created", async () => {
+  const blogsBefore = (await api.get("/api/blogs")).body;
+  const title = "An example blog post";
+
+  await api
+    .post("/api/blogs")
+    .send({
+      title,
+      author: "No One",
+      url: "https://www.example.com",
+      likes: 0,
+    })
+    .expect(201);
+
+  const blogsAfter = (await api.get("/api/blogs")).body;
+  expect(blogsAfter).toHaveLength(blogsBefore.length + 1);
+  expect(blogsAfter.map((b) => b.title)).toContain(title);
+});
