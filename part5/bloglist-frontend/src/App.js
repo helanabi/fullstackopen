3390953574606f95c843e67dcc2e54f03d58fb0f
Hassan El Anabi <helanabi@gmail.com>
2022-06-16 13:24:59 +0100

    finish exercise 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -8,8 +8,12 @@ const App = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    setUser(JSON.parse(window.localStorage.getItem("user")));
   }, []);
+
+  useEffect(() => {
+    if (user) blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, [user]);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -20,13 +24,22 @@ const App = () => {
         event.target.elements[attr].value,
       ])
     );
-    setUser(await loginService.login(credentials));
+    const loggedUser = await loginService.login(credentials);
+    setUser(loggedUser);
+    window.localStorage.setItem("user", JSON.stringify(loggedUser));
+  };
+
+  const logout = () => {
+    setUser(null);
+    window.localStorage.clear();
   };
 
   return user ? (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in <button onClick={logout}>Log out</button>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
