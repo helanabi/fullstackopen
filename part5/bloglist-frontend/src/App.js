2bf7c4a03d304a3a3c3e39b7758b05a5edf6aa1f
Hassan El Anabi <helanabi@gmail.com>
2022-06-17 07:00:14 +0100

    finish exercise 5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -24,8 +24,10 @@ const App = () => {
         event.target.elements[attr].value,
       ])
     );
+
     const loggedUser = await loginService.login(credentials);
     setUser(loggedUser);
+    blogService.setToken(loggedUser.token);
     window.localStorage.setItem("user", JSON.stringify(loggedUser));
   };
 
@@ -34,12 +36,53 @@ const App = () => {
     window.localStorage.clear();
   };
 
+  const createBlog = async (event) => {
+    event.preventDefault();
+
+    const controls = ["title", "author", "url"].map(
+      (name) => event.target.elements[name]
+    );
+
+    const newBlog = Object.fromEntries(
+      controls.map((control) => [control.name, control.value])
+    );
+
+    setBlogs(blogs.concat(await blogService.create(newBlog)));
+
+    controls.forEach((control) => {
+      control.value = "";
+    });
+  };
+
   return user ? (
     <div>
       <h2>blogs</h2>
       <p>
         {user.name} logged in <button onClick={logout}>Log out</button>
       </p>
+      <h2>Create new blog</h2>
+      <form onSubmit={createBlog}>
+        <label>
+          Title:
+          <input type="text" name="title" />
+        </label>
+        <br />
+
+        <label>
+          Author:
+          <input type="text" name="author" />
+        </label>
+        <br />
+
+        <label>
+          URL:
+          <input type="url" name="url" />
+        </label>
+        <br />
+
+        <button type="submit">Create</button>
+      </form>
+      <h2>Created blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,28 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
+
+let token = null;
+
+const setToken = (newToken) => {
+  void token;
+  token = `bearer ${newToken}`;
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-export default { getAll }
+const create = async (blog) => {
+  void token;
+  const res = await axios.post(baseUrl, blog, {
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  return res.data;
+};
+
+const blogService = { setToken, getAll, create };
+export default blogService;
