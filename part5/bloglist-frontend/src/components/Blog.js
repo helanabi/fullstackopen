2eb353c3de852b7d77524fc81e8e24e2ca2ef916
Hassan El Anabi <helanabi@gmail.com>
2022-06-25 06:49:49 +0100

    finish exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expand, setExpand] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "1px solid",
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}{" "}
+      <button onClick={() => setExpand(!expand)}>
+        {expand ? "hide" : "show"}
+      </button>
+      {expand && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
