4bb0bf2a2d71c543992e1d2792d2ad0ec4261c39
Hassan El Anabi <helanabi@gmail.com>
2022-07-14 14:44:25 +0100

    finish exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -1,10 +1,36 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", "http://localhost:3003/api/users", {
+      username: "testuser",
+      name: "Test User",
+      password: "sekret",
+    });
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
     cy.get("form").find("button").contains("Log in");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get('input[name="username"]').type("testuser");
+      cy.get('input[name="password"]').type("sekret");
+      cy.contains("Log in").click();
+      cy.contains("blogs");
+      cy.contains("Test User logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get('input[name="username"]').type("testuser");
+      cy.get('input[name="password"]').type("wrong");
+      cy.contains("Log in").click();
+      cy.contains("password is incorrect").should(
+        "have.css",
+        "color",
+        "rgb(255, 0, 0)"
+      );
+    });
   });
 });
